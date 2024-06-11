9be07638d77ec8948d088e796c87e3fcdcfa8e57
kulkarnianushree <anushreejmkulkarni@gmail.com>
2024-06-11 08:43:50 +0530

    Form Added

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,16 +1,36 @@
 import MealsItem from "./MealsItem"
-const Meals = (props)=>{
-    return(
-        <div>
-            {props.data.map((items)=>{
-                <MealsItem
-                    key ={items.id}
-                    title={items.title}
-                    description = {items.description}
-                    price={items.price}
-                />
+import './Meals.css'
+const Meals = (props) => {
+    return (
+        <div style={{marginLeft:'5%'}}>
+            {props.data.map((items) => {
+                return (
+                    <div className="content">
+                        <MealsItem
+                            key={items.id}
+                            title={items.title}
+                            description={items.description}
+                            price={items.price}
+                        />
+                        <section className="content-extra">
+                            <span className="content-special">
+                                <h5>Amount</h5>
+                                <h6> 0 </h6>
+                            </span>
+                            
+                            <span>
+                                <button>+Add</button>
+                            </span>
+                        </section>
+                    
+                    </div>
+                
+                );
+
             })}
+
         </div>
-    )
+    );
 }
-export default Meals
+
+export default Meals;
